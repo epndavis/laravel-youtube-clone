6ab53002fe01541d46425dbd1d2e1cf2aa4eed5c
epndavis <47059262+epndavis@users.noreply.github.com>
2020-06-04 18:39:45 +0100

    Get id from user factory in video factory

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -6,10 +6,10 @@ use App\Models\User;
 use App\Models\Video;
 use Faker\Generator as Faker;
 
-$factory->define(Video::class, function (Faker $faker) {
+$factory->define(Video::class, function (Faker $faker) use ($factory) {
     return [
         'title' => $faker->text(180),
         'description' => $faker->sentences(3),
-        'uploaded_by' => factory(User::class)
+        'uploaded_by' => $factory->create(User::class)->id
     ];
 });
