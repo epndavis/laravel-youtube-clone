cb5ed7eae70aaaf003cc0054f93bb44ef65534fe
epndavis <47059262+epndavis@users.noreply.github.com>
2020-06-04 18:27:02 +0100

    Add api folder and video api controller resource

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('/videos')->group(function() {
+    Route::get('/', 'Api\VideoController@index')->name('video.index');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
