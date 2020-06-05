0df7bf766b1f76ffce3406a85aa23c8ed68ba494
epndavis <47059262+epndavis@users.noreply.github.com>
2020-06-05 18:10:33 +0100

    Add video store api tests

--- tests/Feature/VideoApiTest.php
+++ tests/Feature/VideoApiTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Video;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class VideoApiTest extends TestCase
@@ -62,12 +63,52 @@ class VideoApiTest extends TestCase
         ]);
 
         $this->json('GET', '/api/videos/' . $video->id, [])
-            ->assertStatus(200)
+            ->assertOk()
             ->assertJson([
                 'id' => $video->id,
                 'title' => 'Example title', 
                 'description' => 'Example description' ,
             ])
             ->assertJsonStructure(['id', 'title', 'description', 'uploaded_by', 'created_at', 'updated_at']);
+    }
+
+    /**
+     * @test
+     */
+    function can_store_video()
+    {
+        $user = factory(User::class)->create();
+
+        $file = UploadedFile::fake()->image('vidoe.png');
+
+        $this->json('POST', route('video.store'), [
+            'title' => 'Example Title',
+            'description' => 'Example Description',
+            'uploaded_by' => $user->id,
+            'file' => $file,
+        ])
+        ->assertStatus(200);
+
+        $video = Video::with('media')->first();
+
+        $this->assertFileExists($video->media->first()->getPath());
+    }
+
+    /**
+     * @test
+     */
+    function throws_error_if_no_data_is_given()
+    {
+        $this->json('POST', route('video.store'), []) 
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'errors',
+            ])
+            ->assertJsonValidationErrors([
+                'title',
+                'description',
+                'file',
+            ]);
     }
 }
