c6b4c0b6be42951225d7598a5bc9048b8cb15bcb
epndavis <47059262+epndavis@users.noreply.github.com>
2020-06-06 17:23:35 +0100

    Update tests to include api resource returns

--- tests/Browser/HomePageTest.php
+++ tests/Browser/HomePageTest.php
@@ -2,12 +2,17 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Laravel\Dusk\Browser;
+use App\Models\User;
 use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class HomePageTest extends DuskTestCase
 {
+    use RefreshDatabase;
+    
     /**
      * @test
      *
@@ -18,6 +23,32 @@ class HomePageTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                     ->assertSee('Youtube Clone');
+        });
+    }
+
+    /** 
+     * @test
+     */
+    public function can_click_through_to_video() 
+    {
+        factory(User::class)->create();
+        
+        $videoFile = UploadedFile::fake()->image('video.png');
+
+        Storage::fake(config('media-library.disk_name'));
+
+        $this->json('POST', route('video.store'), [
+            'title' => 'Example Title',
+            'description' => 'Example Description',
+            'video' => $videoFile,
+        ]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->waitFor('a h3')
+                    ->click('a h3')
+                    ->waitFor('#video')
+                    ->assertSee('Example Description');
         });
     }
 }
