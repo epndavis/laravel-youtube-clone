d63ec3f86053dee21a6c101a4288af1940e62857
epndavis <47059262+epndavis@users.noreply.github.com>
2020-06-06 12:26:22 +0100

    Install laravel dusk

---
+++ tests/Browser/HomePageTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function can_load_homepage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+}
